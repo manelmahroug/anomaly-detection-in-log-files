7d4312cbd8a765f6d855e73ec83b93c7610d3e94
tps1316 <98610486+tps1316@users.noreply.github.com>
2023-07-17 23:10:34 -0700

    Create clusters2.csv after tfidf filtering in all_params.csv

--- parse/ad_feature_extraction.py
+++ parse/ad_feature_extraction.py
@@ -17,8 +17,8 @@ from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn.metrics import calinski_harabasz_score, davies_bouldin_score
 import uuid
 import matplotlib.pyplot as plt
-import math
 import subprocess
+import random
 
 BASE_DIR = '/home/thanuja/Dropbox/capstone/raw_files'
 SUB_DIR = ['BGL','Hadoop','OpenStack','Thunderbird']
@@ -47,8 +47,6 @@ def mark_anomalies():
                         open_stack_anomalies.add(line.strip())
     return hadoop_anomalies, open_stack_anomalies
 
-            
-hadoop_anomalies, open_stack_anomalies = mark_anomalies()
 
 # parses timestamp and writes intial cluster CSVs
 def process_raw_files(split_test_train=False):
@@ -65,11 +63,11 @@ def process_raw_files(split_test_train=False):
                 file_name = os.path.join(root,file)
                 print(file_name)
                 app_sys_name = re.search(r'raw_files/([^/]+)/',file_name).group(1)
-                csvfile_with_ts = parse_ts(file_name, app_sys_name)
+                csvfile_with_ts = parse_ts_sample(file_name, app_sys_name)
+                #csvfile_with_ts = parse_ts(file_name, app_sys_name)
                 csvs_with_ts.add(csvfile_with_ts)
     for csvfile_with_ts in csvs_with_ts:
-        get_clusters(csvfile_with_ts, split_test_train=True)
-        get_clusters(csvfile_with_ts, split_test_train=False)
+        get_clusters(csvfile_with_ts, split_test_train=split_test_train)
 
 # processes all cluster output files to create a sliding window file for each.
 def apply_sliding_window():
@@ -248,7 +246,7 @@ parsers = {'Hadoop':parse_hadoop_files,'OpenStack':parse_openstack_files,'BGL':p
 def parse_ts(file_name,app_sys_name):
     num_anomalies = int(subprocess.run(['grep', '-cv', '^-', file_name], stdout=subprocess.PIPE).stdout)
     num_normal = int(subprocess.run(['grep', '-c', '^-', file_name], stdout=subprocess.PIPE).stdout)
-    anomaly_target_percent = num_anomalies / num_normal
+    anomaly_target_percent = num_anomalies / (num_anomalies + num_normal)
     # Some slack is needed otherwise algorithm rarely adds anomalous or non-anomalous rows
     max_anomaly_percent = anomaly_target_percent * 1.1
     min_anomaly_percent = anomaly_target_percent * 0.9
@@ -366,15 +364,15 @@ def get_clusters(csv_log_file, split_test_train=False):
     num_rows = tvec_weights_train.shape[0]
     best_score = 0
     best_kmeans = None
-    k = 20
+    k = 10
     if num_rows > max_rows:
         tvec_weights_sample = tvec_weights_train[np.random.choice(num_rows, max_rows, replace=False), :]
     else:
         tvec_weights_sample = tvec_weights_train
     # automatically determining optimal k. Repeat k-means until small_cluster_size < max_smallest_cluster_size
     while k < 100 or smallest_cluster_size > max_smallest_cluster_size:
-        #kmeans = KMeans(n_clusters=k, n_init=5, algorithm='elkan').fit(tvec_weights_sample)
-        kmeans = MiniBatchKMeans(n_clusters=k, n_init=5, batch_size=10000).fit(tvec_weights_sample)
+        #kmeans = KMeans(n_clusters=k, n_init=10, algorithm='elkan').fit(tvec_weights_sample)
+        kmeans = MiniBatchKMeans(n_clusters=k, n_init=10, batch_size=10000).fit(tvec_weights_sample)
         labels = kmeans.predict(tvec_weights_sample)
         dense_weights = tvec_weights_sample.toarray()
         ch_score = calinski_harabasz_score(dense_weights, labels)
@@ -385,7 +383,7 @@ def get_clusters(csv_log_file, split_test_train=False):
             best_kmeans = kmeans
         smallest_cluster_size = min(np.bincount(kmeans.labels_))
         print('k', k, 'smallest cluster', smallest_cluster_size, 'ch', ch_score, 'db', db_score, 'score', score)
-        k+=10
+        k=int(k*1.1)+1
     train_labels = best_kmeans.predict(tvec_weights_train)
     clusters_train_df = pd.DataFrame({'clusters': train_labels})
     result_train_df = pd.concat([train_df, clusters_train_df], axis=1, join='inner')
@@ -409,8 +407,10 @@ def dataset_balance():
         plt.show()
 
 def simple_tokenizer(line):
-    for c in '[](){}/,':
-        line  = line.replace(c, '')
+    for c in '[](){},':
+        line = line.replace(c, '')
+    for c in '=/:':
+        line = line.replace(c, ' ')
     return line.split(" ")
 
 def get_params_within_cluster():
@@ -421,20 +421,24 @@ def get_params_within_cluster():
         print(file)
         cluster_df = pd.read_csv(OUTPUT_DIR + file,index_col=[0])
         num_clusters = max(cluster_df.clusters) + 1
+        cluster_df["tfidf_text"] = cluster_df["text"]
         
         for i in range(num_clusters):
             sub_df = cluster_df[cluster_df["clusters"] == i]
-            tvec = TfidfVectorizer(min_df=1, max_df = 10, tokenizer=simple_tokenizer)
+            tfidf_filter = TfidfVectorizer(min_df=1, max_df = 0.05, tokenizer=simple_tokenizer)
+            tfidf_extractor = TfidfVectorizer(min_df=1, max_df = 0.5, tokenizer=simple_tokenizer)
             try:
-                tvec.fit_transform(sub_df.text)
+                tfidf_filter.fit_transform(sub_df.text)
+                tfidf_extractor.fit_transform(sub_df.text)
             except:
                 print('No parameters for ', sub_df["text"].head(1))
                 continue
             
-            print('tvec_vocab: ',len(tvec.get_feature_names_out()))
+            print('cluster', i, 'filter words', len(tfidf_filter.get_feature_names_out()),
+                  'extractor words', len(tfidf_extractor.get_feature_names_out()))
             count = 0
             for index, row in sub_df.iterrows():
-                words = row["text"].split(" ")
+                words = simple_tokenizer(row["text"])
                 p = 0
                 count += 1
                 if count % 10000 == 0:
@@ -443,14 +447,23 @@ def get_params_within_cluster():
                 #print('index',index, row)
                 for j, word in enumerate(words):
                     # Remove if it has any digits. Assume all character words are meaningful.
-                    if word in tvec.vocabulary_: # and bool(re.search(r'\d', word)):
-                        cluster_df.at[index, 'text'] = cluster_df.at[index, 'text'].replace(word, '')
+                    if word in tfidf_filter.vocabulary_: # and bool(re.search(r'\d', word)):
+                        cluster_df.at[index, 'tfidf_text'] = cluster_df.at[index, 'tfidf_text'].replace(word, '')
+
+                    # Convert hex to int string
+                    if re.match(r'^0x[0-9abcdef]+$', word):
+                        cluster_df.at[index, 'tfidf_text'] = cluster_df.at[index, 'tfidf_text'].replace(word, '')
+                        word = str(int(word, 16))
+                    # Only extract decimal
                     if not word.replace('.', '').isnumeric():
                         continue
-                    pattern = re.match(r'(\d*\.\d*\.)(\d*\.)*', word)
-                    if pattern:
+
+                    # Filter numbers and ip addresses
+                    cluster_df.at[index, 'tfidf_text'] = cluster_df.at[index, 'tfidf_text'].replace(word, '')
+                    
+                    if re.match(r'(\d*\.\d*\.)(\d*\.)*', word):
                         continue
-                    if word in tvec.vocabulary_:
+                    if word in tfidf_extractor.vocabulary_:
                         cluster_df.at[index, "p-"+str(i)+"-"+str(p)] = word
                         #if count < 3: print('\tFound param "%s" in row %d at position %d(p=%d) in cluster %d' % (word, row[0], j, p, i))
                         p += 1
@@ -459,19 +472,121 @@ def get_params_within_cluster():
         cluster_df.to_csv(outfile, quoting=csv.QUOTE_NONNUMERIC)
 
 
-def second_clusters():
+def parse_ts_sample(file_name, app_sys_name):
+    SAMPLE_SIZE = 100000
+    SAMPLE_ANOMALY_SIZE = 5000
+    CHUNK_SIZE = 1000
+    file_path = file_name.split('/')
+    lines = []
+    num_anomalies = int(subprocess.run(['grep', '-cv', '^-', file_name], stdout=subprocess.PIPE).stdout)
+    num_normal = int(subprocess.run(['grep','-cv','^_',file_name],stdout=subprocess.PIPE).stdout)
+    total_lines = num_anomalies + num_normal
+    output_line_perc = SAMPLE_SIZE/total_lines
+    output_anomaly_perc = SAMPLE_ANOMALY_SIZE/num_anomalies
+    total_output_percentage = max(output_line_perc,output_anomaly_perc)
+    log_file = open(file_name, "r+", encoding="utf8", errors='ignore')
+    out_file = OUTPUT_DIR+app_sys_name+'.csv'
+    is_file = os.path.isfile(out_file)
+    csv_file = open(out_file, 'a+')
+    anomaly_output_count = 0
+    line_output_count = 0
+    line_read_count = 0
+    if app_sys_name not in parsers:
+        raise ValueError("no parser found for " + app_sys_name)
+    run_parser = parsers[app_sys_name]
+    with csv_file,log_file:
+        csvwriter = csv.writer(csv_file, quoting=csv.QUOTE_NONNUMERIC, escapechar='\\')
+        if not is_file:
+            csvwriter.writerow(['timestamp','text','label','filename'])
+        for line in log_file:
+            line = line.rstrip('\n')
+            line_read_count += 1
+            lines.append(line)
+            if len(lines) == CHUNK_SIZE:
+                r = random.random()
+                #print(r, total_output_percentage)
+                if r < total_output_percentage:
+                    #print(r)
+                    prev = None
+                    for block_line in lines:
+                        try:
+                            epochts,remainder_ll,is_anomaly,filename = run_parser(file_path, block_line)
+                            if is_anomaly:
+                                anomaly_output_count +=1;
+                        except ValueError:
+                            # when the line does not follow the normal format, assume it is continuation of 
+                            # previous log line. Errors than span multiple lines, are put together in a single line.
+                            prev[1] += block_line # 1 is the column of the text
+                            continue
+                        if prev is not None:
+                            # only write a line when there is no chance more data will be added to it.
+                            # so when the current line parses successfully, the previous line must be complete.
+                            csvwriter.writerow(prev)
+                        prev = [epochts,remainder_ll,is_anomaly,filename]
+                    csvwriter.writerow(prev)
+                    line_output_count += 1000
+                    if line_output_count % 10000:
+                        print(anomaly_output_count, line_output_count, line_read_count)
+                lines = []
+    print('line_output_count ',line_output_count)
+    print('anomaly_output_count ',anomaly_output_count)
+    return out_file
+
+def redo_clusters():
     cluster_files = os.listdir(OUTPUT_DIR)
     for file in cluster_files:
         if not file.endswith('_all_params.csv'):
             continue
-        get_clusters(OUTPUT_DIR+file, split_test_train=False)
-        get_clusters(OUTPUT_DIR+file, split_test_train=True)
+        print(file)
+        cluster_df = pd.read_csv(OUTPUT_DIR + file,index_col=[0])
+        num_clusters = max(cluster_df.clusters) + 1
+        X = cluster_df.drop(columns = ['label','timestamp','filename'])
+    
+        tfidf = TfidfVectorizer()
+        X_tfidf = tfidf.fit_transform(X['tfidf_text'])
+    
+        best_score = 0
+        best_kmeans = None
+        k = num_clusters - 5
+        # automatically determining optimal k. Repeat k-means until small_cluster_size < max_smallest_cluster_size
+        while k < num_clusters + 5:
+            #kmeans = KMeans(n_clusters=k, n_init=10, algorithm='elkan').fit(tvec_weights_sample)
+            kmeans = MiniBatchKMeans(n_clusters=k, n_init=10, batch_size=10000).fit(X_tfidf)
+            labels = kmeans.predict(X_tfidf)
+            if X_tfidf.shape[0] > 50000:
+                indices = np.random.choice(X_tfidf.shape[0], 50000, replace=False)
+                dense_weights = X_tfidf[indices].toarray()
+                dense_labels = labels[indices]
+            else:
+                dense_weights = X_tfidf.toarray()
+                dense_labels = labels
+            ch_score = calinski_harabasz_score(dense_weights, dense_labels)
+            db_score = davies_bouldin_score(dense_weights, dense_labels)
+            score = ch_score / (10000 * db_score * db_score)
+            if best_kmeans is None or best_score < score:
+                best_score = score
+                best_kmeans = kmeans
+            smallest_cluster_size = min(np.bincount(kmeans.labels_))
+            print('k', k, 'smallest cluster', smallest_cluster_size, 'ch', ch_score, 'db', db_score, 'score', score)
+            k+=1
+    
+        kmeans = best_kmeans
+        cluster_seq = kmeans.predict(X_tfidf)
+        cluster_df['cluster2'] = cluster_seq
+        
+        outfile = OUTPUT_DIR + file.replace('_all_params.csv', '_clusters2.csv')
+        cluster_df.to_csv(outfile, quoting=csv.QUOTE_NONNUMERIC)
 
-#get_clusters(OUTPUT_DIR+'Thunderbird_all_params.csv')
-process_raw_files()
-get_params_within_cluster()
-second_clusters()
-#apply_sliding_window()
-#dataset_balance()
-print('DONE')
-   
+
+if __name__ == '__main__':
+    random.seed(0)
+    #hadoop_anomalies, open_stack_anomalies = mark_anomalies()
+
+    #get_clusters(OUTPUT_DIR+'BGL_all_params.csv')
+    #parse_ts_sample('/home/thanuja/Dropbox/capstone/raw_files/BGL/BGL.log','BGL')
+    process_raw_files(split_test_train=False) # for unsupervised training
+    #process_raw_files(split_test_train=True) # for supervised training
+    get_params_within_cluster()
+    redo_clusters()
+    #dataset_balance()
+    print('DONE')
